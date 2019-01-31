97e830c42fcc2a6887fded28998473774221cc7b
AlaaMostapha <alaamoustapha21@gmail.com>
2019-01-31 12:49:45 +0200

    Add files via upload

---
+++ js/customjsfunctions.js
@@ -0,0 +1,37 @@
+var w1 = document.getElementById("w-1");
+var w2 = document.getElementById("w-2");
+var w3 = document.getElementById("w-3");
+var w4 = document.getElementById("w-4");
+
+
+function hover1() {
+    w1.setAttribute('src', 'images/partners/c-1.png');
+}
+
+function unhover1() {
+    w1.setAttribute('src', 'images/partners/w-1.png');
+}
+
+
+function hover2(){
+    w2.setAttribute('src', 'images/partners/c-2.png');
+}
+function unhover2(){
+  w2.setAttribute('src', 'images/partners/w-2.png');
+}
+
+
+function hover3(){
+    w3.setAttribute('src', 'images/partners/c-3.png');
+}
+function unhover3(){
+  w3.setAttribute('src', 'images/partners/w-3.png');
+}
+
+
+function hover4(){
+    w4.setAttribute('src', 'images/partners/c-4.png');
+}
+function unhover4(){
+  w4.setAttribute('src', 'images/partners/w-4.png');
+}
